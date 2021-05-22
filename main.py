d3e5b27ec32fe18fa990641dd3503c820e5d5a6d
Jakub Ciszek <jakubciszek64@gmail.com>
2021-05-22 17:53:48 +0200

    Working example

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello there')
+# %%
+import pytorch_lightning as pl
+
+from train.datamodules import TESSDatamodule
+from train.models import TESSLinearModel
+
+pl.seed_everything(42)
+
+m = TESSLinearModel()
+dm = TESSDatamodule(256)
+
+t = pl.Trainer(gpus=1, max_epochs=100)
+t.fit(m, datamodule=dm)
+# %%
