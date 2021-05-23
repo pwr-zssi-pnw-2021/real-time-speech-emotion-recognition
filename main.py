5596d7d28ea0a7570696d39d5837f1e8ab3e309a
Jakub Ciszek <jakubciszek64@gmail.com>
2021-05-23 15:54:22 +0200

    Conv showcase

--- main.py
+++ main.py
@@ -2,12 +2,17 @@
 import pytorch_lightning as pl
 
 from train.datamodules import TESSDatamodule
-from train.models import TESSLinearModel
+from train.models import TESSConvModel, TESSLinearModel
 
 pl.seed_everything(42)
 
-m = TESSLinearModel()
-dm = TESSDatamodule(256)
+# Linear
+# m = TESSLinearModel()
+# dm = TESSDatamodule(256)
+
+# Conv
+m = TESSConvModel()
+dm = TESSDatamodule(256, data_dim=2)
 
 t = pl.Trainer(gpus=1, max_epochs=100)
 t.fit(m, datamodule=dm)
