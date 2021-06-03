d949a4a162b9830cd82ed62596486ccba206ceed
Jakub Ciszek <jakubciszek64@gmail.com>
2021-06-03 23:35:59 +0200

    Feature split on global index

--- preprocess/combine_index.py
+++ preprocess/combine_index.py
@@ -7,15 +7,35 @@ if __name__ == '__main__':
     params = get_params()
 
     index_dir = Path(params['data']['index_dir'])
-    global_index = Path(params['data']['global_index'])
 
-    all_paths = []
-    all_classes = []
+    global_index_dir = Path(params['data']['global_index_dir'])
+    global_index_dir.mkdir(exist_ok=True)
+
+    features = params['data']['features']
+    index_dic = {
+        f: {
+            'paths': [],
+            'classes': [],
+        }
+        for f in features
+    }
+
     for path in index_dir.glob('*'):
+        ft = path.stem.split('_')[-1]
         with open(path, 'rb') as f:
             paths, classes = pkl.load(f)
-        all_paths.extend(paths)
-        all_classes.extend(classes)
 
-    with open(global_index, 'wb') as f:
-        pkl.dump((all_paths, all_classes), f, pkl.HIGHEST_PROTOCOL)
+        index_dic[ft]['paths'].extend(paths)
+        index_dic[ft]['classes'].extend(classes)
+
+    for f_name, idx in index_dic.items():
+        idx_path = global_index_dir / f_name
+        with open(idx_path, 'wb') as f:
+            pkl.dump(
+                (
+                    idx['paths'],
+                    idx['classes'],
+                ),
+                f,
+                pkl.HIGHEST_PROTOCOL,
+            )
